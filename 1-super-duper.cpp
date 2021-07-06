ae37a8770b3441d2d8381508691fca751e1af6b0
ferrydrmn <69904480+ferrydrmn@users.noreply.github.com>
2021-07-06 08:52:03 +0700

    Update 1-super-duper.cpp
    - Penghapusan variabel const n_min
    - Penambahan spasi pada output "Energi yang dihasilkan "untuk merapikan output
    - Penambahan beberapa komentar

--- 1-super-duper.cpp
+++ 1-super-duper.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 //Kamus
-const int n_min = 2; //variabel batas minimal energi adalah 2
-int n;
-int energi;
+int n;              //Input awal pengguna
+int energi;         //variabel size array, 2n - 1
 int sum_energi = 0; //variabel output untuk jumlah energi
 int i = 0;          //variabel perulangan
 int j = 0;          //variabel pengubahan energi yang bisa dilakukan
@@ -13,26 +12,26 @@ int main()
 {
     cout << "Masukkan n: ";
     cin >> n;
-    if (n >= n_min)
+    if (n >= 2) //jika kurang dari 2, maka energi (size array) akan bernilai nol dan tidak ada yang dapat diproses oleh program
     {
         energi = 2 * n - 1;
         int *arr = new int(energi); //variabel array dinamis
-        for (i = 0; i < energi; i++)
+        for (i = 0; i < energi; i++)    //memasukkan nilai energi ke array dinamis
         {
             cout << "Masukkan energi ke-" << i + 1 << ": ";
             cin >> arr[i];
         }
-        sort(arr, arr + energi);
-        for (i = 0; i < n; i++)
+        sort(arr, arr + energi);    //mengurutkan array dari yang terkecil, agar dapat memaksimalkan energi negatif yang diubah
+        for (i = 0; i < n; i++)     //pengubahan energi negatif ke positif sejumlah input n, lalu dijumlahkan kumulatifnya
         {
             arr[i] *= -1;
             sum_energi += arr[i];
         }
-        for (i = n; i < energi; i++)
+        for (i = n; i < energi; i++)    //penambahan sisa energi lain
         {
             sum_energi += arr[i];
         }
-        cout << "Energi yang dihasilkan" << sum_energi;
+        cout << "Energi yang dihasilkan " << sum_energi;    //output energi
     }
     else
     {
